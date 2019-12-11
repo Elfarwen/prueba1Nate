acfa007587ee5ffd8d629027e84c998f540fb0e3
Elfarwen <58736072+elfarwen@users.noreply.github.com>
2019-12-11 15:35:22 +0100

    Ejercicio de adivina un numero con cinco intentos max.

---
+++ adivina_un_numero.py
@@ -0,0 +1,35 @@
+number_to_guess = 7
+user_number = int(input("Adivina un numero: "))
+
+if number_to_guess == user_number:
+    print("Has ganado")
+else:
+    print("Prueba de nuevo")
+
+user_number = int(input("Adivina un numero: "))
+
+if number_to_guess == user_number:
+    print("Has ganado")
+else:
+    print("Prueba de nuevo")
+
+user_number = int(input("Adivina un numero: "))
+
+if number_to_guess == user_number:
+    print("Has ganado")
+else:
+    print("Prueba de nuevo")
+
+user_number = int(input("Adivina un numero: "))
+
+if number_to_guess == user_number:
+    print("Has ganado")
+else:
+    print("Prueba de nuevo")
+
+user_number = int(input("Adivina un numero: "))
+
+if number_to_guess == user_number:
+    print("Has ganado")
+else:
+    print("Has perdido")
